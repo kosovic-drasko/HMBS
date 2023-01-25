09cbf28261281a443137114f99c751e456907ed5
kosovic-drasko <kosovic.drasko@montefarm.co.me>
2023-01-25 19:06:11 +0100

    Initial version of HMBS generated by generator-jhipster@7.8.1

---
+++ src/main/java/hmbs/config/ApplicationProperties.java
@@ -0,0 +1,12 @@
+package hmbs.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+/**
+ * Properties specific to HMBS.
+ * <p>
+ * Properties are configured in the {@code application.yml} file.
+ * See {@link tech.jhipster.config.JHipsterProperties} for a good example.
+ */
+@ConfigurationProperties(prefix = "application", ignoreUnknownFields = false)
+public class ApplicationProperties {}
---
+++ src/main/java/hmbs/service/dto/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Data Transfer Objects.
+ */
+package hmbs.service.dto;
